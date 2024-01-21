Fixed major normalization error

data-conversion.py
@@ -46,9 +46,10 @@ that contain the object. This allows more information to be
 preserved in the reduced precision.
 """
 
-reduced_img = img - 9600
+reduced_img = img - 9500
 reduced_img[reduced_img<0] = 0  # Removes nonnegative values, sets to zero.
-reduced_img = reduced_img / np.max(reduced_img)  # Normalize to [0,1]
+reduced_img[reduced_img>3500] = 3500  # Removes weird outliers that I can't locate
+reduced_img = reduced_img / 3500  # Normalize to [0,1]
 reduced_img = reduced_img * 255  # Scale to unsigned 8 bit integers
 reduced_img = np.array(reduced_img, dtype=np.uint8)  # Convert type to uint8
 